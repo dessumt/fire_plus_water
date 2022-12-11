82b40ded30255d018b94e29958b680ed049546a0
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2022-12-11 12:07:33 +0300

    a bit more lights and improving code readability

--- light.py
+++ light.py
@@ -6,24 +6,24 @@ r = 5
 EMBIANT_COLOR = (255, 247, 0, 230)
 REFLECT_COLOR = (237, 204, 38, 220)
 
-def find_light_collision(mask, v, mouse_pos, step, cat):
+def find_light_collision(v, mouse_pos, step, mask):
     if step == 0:
         return mouse_pos
-    for i in range(5, 1000, step):
+    for i in range(5, 400, step):
         cords = (mouse_pos[0] + v[0]*i, mouse_pos[1] + v[1]*i)
-        pos_in_mask = cords[0] - cat.rect.x, cords[1] - cat.rect.y
-        touching = cat.rect.collidepoint(*cords) and cat.mask.get_at(pos_in_mask)
+        pos_in_mask = cords[0] - mask.rect.x, cords[1] - mask.rect.y
+        touching = mask.rect.collidepoint(*cords) and mask.mask.get_at(pos_in_mask)
         if touching:
-            prev_cords = mouse_pos[0] + v[0]*(i-step), mouse_pos[1] + v[1]*(i-step)
-            return find_light_collision(mask, v, prev_cords, step//2, cat)
-    return mouse_pos[0] + v[0]*1000, mouse_pos[1] + v[1]*1000
+            prev_cords = mouse_pos[0] + v[0]*(i-2*step), mouse_pos[1] + v[1]*(i-2*step)
+            return find_light_collision(v, prev_cords, step//2, mask)
+    return mouse_pos[0] + v[0]*400, mouse_pos[1] + v[1]*400
 
-def cast_light_line(Surface2draw, v, mask, mouse_pos, cat, step=10):
-    cords = find_light_collision(mask, v, mouse_pos, step, cat)
+def cast_light_line(Surface2draw, v,  mouse_pos, mask, step=10):
+    cords = find_light_collision(v, mouse_pos, step, mask)
     pygame.draw.line(Surface2draw, EMBIANT_COLOR, (mouse_pos[0], mouse_pos[1]), cords, 4)
     # pygame.draw.circle(Surface2draw, REFLECT_COLOR, cords, 6)
 
-def cast_all_lights(Surface2draw, mask, mouse_pos, cat):
-    for a in range(0, 36000,20):
+def cast_all_lights(Surface2draw, mouse_pos, mask):
+    for a in range(0, 36000,15):
         v = (math.sin(a/(2*math.pi/100)), math.cos(a/(2*math.pi/100)))
-        cast_light_line(Surface2draw, v, mask, mouse_pos, cat)
+        cast_light_line(Surface2draw, v,  mouse_pos, mask)
