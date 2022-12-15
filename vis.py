5e4808926b24991a1f021c915cdf5b4bc321b46d
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2022-12-15 17:11:39 +0300

    Добавлена  кнопка
    Добавлена кнопка и взаимодействие персонажа с кнопкой

--- vis.py
+++ vis.py
@@ -1,12 +1,19 @@
 # огонь и вода
 import pygame
 from light import *
+from flame import *
+from character import *
+from button import *
 
+
+EMBIANT_COLOR = [(213, 169 + i, 21, 230) for i in range(25)]
 pygame.init()
 
-WIDTH = 1920
-HEIGHT = 1080
+WIDTH = 1000
+HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
 class Mask:
     def __init__(self, filename, scale):
         self.image = pygame.image.load(filename).convert_alpha()
@@ -21,20 +28,22 @@ def put_light_source(surf, mask, pos, color):
     surf.set_alpha(120)
     cast_all_lights(surf, pos, mask, color)
     surf.blit(light_surf, (pos[0]-WIDTH / 2, pos[1]-HEIGHT / 2))
-    # screen.blit(surf, (0,0))
+    screen.blit(surf, (0,0))
+
+    
 
 light_surf = pygame.image.load('light_mask.png').convert_alpha()
 light_surf = pygame.transform.scale(light_surf, (WIDTH, HEIGHT))
 
 screen.fill((0,0,0))
 running = True
-mask = Mask('map.png', (WIDTH-325, HEIGHT-215))
-all_pos = [(100, 100), (200, 275), (350, 375)]
+mask = Mask('map.png', (WIDTH, HEIGHT))
+all_pos = [(100, 100), (200, 275), (375, 375)]
 flame = [Flame(pos[0], pos[1]) for pos in all_pos]
 motion = None
 character = Character()
-surf = pygame.Surface((WIDTH, HEIGHT))
-surf.set_colorkey((0,0,0))
+button_pos = (320, 417)
+
 
 while running:
     screen.fill((0,0,0))
@@ -60,7 +69,10 @@ while running:
     for i in range(len(all_pos)):
         put_light_source(surf, mask, all_pos[i], EMBIANT_COLOR[random.randint(1, 15)])
         flame[i].draw_flame()
+
     character.move(motion)
     character.draw(surf, screen, color)
+    button = Button(button_pos, mask, screen)
+    button.is_button_pressed(character)
 
     pygame.display.update()
