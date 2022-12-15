fb8cab6e6d4881dbd6b855201a8872a6ea267b02
Darya <114287193+dessumt@users.noreply.github.com>
2022-12-15 22:52:54 +0300

    Update Character.py

--- Character.py
+++ Character.py
@@ -17,13 +17,15 @@ class Character:
     def move(self, motion):
         jumpcount = 0
         # по горизонтали
-        if motion == 'LEFT' and self.character_pos[0]>55:
+        if motion == 'LEFT' and not self.is_touching[0]:
             # self.character_pos[0] -= 5
             self.vx = -5
-        elif motion == 'RIGHT' and self.character_pos[0]<930:
+        elif motion == 'RIGHT' and not self.is_touching[1]:
             # self.character_pos[0] += 5
             self.vx = 5
-        else:
+        elif self.is_touching[0] or self.is_touching[1]:
+            self.vx = 0
+        elif not motion:
             self.vx = 0
         self.character_pos[0] += self.vx
 
@@ -38,7 +40,7 @@ class Character:
         if motion == 'UP' and self.is_touching[3]:
             # self.character_pos[1] += (jumpcount ** 2) / 3
             # self.character_pos[1] -= (jumpcount**2)/3
-            self.vy = 5
+            self.vy = 6
             self.is_touching[3] = False
             # jumpcount -= 1
             # self.vy += jumpcount/3
@@ -47,6 +49,8 @@ class Character:
             self.vy -= jumpcount/3
         elif self.is_touching[3]:
             self.vy = 0
+        if self.is_touching[2]:
+            self.vy *= -1
 
         self.character_pos[1] -= self.vy
         self.hitbox = pygame.Rect(self.character_pos[0], self.character_pos[1], self.hitbox_size, self.hitbox_size)
@@ -73,32 +77,42 @@ class Character:
         for i, cords in enumerate(self.phisical_hitbox_points):
             pos_in_mask = cords[0] - mask.rect.x, cords[1] - mask.rect.y
             touching = mask.rect.collidepoint(*cords) and mask.mask.get_at(pos_in_mask)
-            if touching == True:
+            if touching:
                 if i == 0:
                     # касание левой частью хитбокса
-                    self.is_touching = [True, False, False, False]
-                elif i == 3:
+                    self.is_touching[0] = True
+                if i == 3:
                     # касание правой частью
-                    self.is_touching = [False, True, False, False]
-                elif i == 1:
+                    self.is_touching[1] = True
+                if i == 1:
                     # касание верхней частью
-                    self.is_touching = [False, False, True, False]
-                elif i == 2:
+                    self.is_touching[2] = True
+                if i == 2:
                     # касание нижней
-                    self.is_touching = [False, False, False, True]
-                break
-        if touching:
-            if self.is_touching == [True, False, False, False]:
-                self.vx = 0
-            if self.is_touching == [False, True, False, False]:
-                self.vx = 0
-            if self.is_touching == [False, False, True, False]:
-                self.vy = 0
-            if self.is_touching == [False, False, False, True]:
-                self.vy = 0
+                    self.is_touching[3] = True
+            else:
+                if i == 0:
+                    # касание левой частью хитбокса
+                    self.is_touching[0] = False
+                if i == 3:
+                    # касание правой частью
+                    self.is_touching[1] = False
+                if i == 1:
+                    # касание верхней частью
+                    self.is_touching[2] = False
+                if i == 2:
+                    # касание нижней
+                    self.is_touching[3] = False
+        if self.is_touching:
+        #     if self.is_touching == [True, False, False, False]:
+        #         self.vx = 0
+        #     if self.is_touching == [False, True, False, False]:
+        #         self.vx = 0
+        #     if self.is_touching == [False, False, True, False]:
+        #         self.vy = 0
+        #     if self.is_touching == [False, False, False, True]:
+        #         self.vy = 0
             color = (255, 255, 255)
         else:
-            self.is_touching = [False, False, False, False]
             color = (255,0,0,200)
         return color
-
